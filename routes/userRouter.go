df693aa250cf7757dc2429859601af9cc66a4bce
d7 <dhruvitpatel2811@gmail.com>
2024-01-26 20:14:29 +0530

    ...

---
+++ routes/userRouter.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	controller "go-jwt-project/controller"
+	"go-jwt-project/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+
+}
